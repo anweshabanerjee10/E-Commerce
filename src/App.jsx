28ab2e82a908423d34baf41cc9d447c2d9d29e73
Anwesha <anwesha.banerjee.cse23@heritageit.edu.in>
2023-01-06 21:13:10 +0530

    Login and register added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 // import Home from "./pages/Home";
 
-import Product from "./pages/Product";
+import Login from "./pages/Login";
+
+// import Register from "./pages/Register";
+
+// import Product from "./pages/Product";
 
 // import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Product />;
+  return <Login />;
 };
 
 export default App;
